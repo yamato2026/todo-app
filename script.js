0a42993208eb040b2671fed63ae7f6727b196c69
yamato <mitsu.daigaku@gmail.com>
2026-06-02 14:25:21 +0900

    Update script.js

--- script.js
+++ script.js
@@ -128,15 +128,4 @@ function saveTodo(){
 }
 
 renderTodos();
-.complete-btn{
-    width:100%;
-    margin-top:10px;
-    border:none;
-    border-radius:12px;
-    padding:12px;
-    background:#28a745;
-    color:white;
-    font-size:16px;
-    font-weight:bold;
-    cursor:pointer;
-}
+
