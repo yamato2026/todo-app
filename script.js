0d184eeab8baf28cdf43aafa4d242e4c8fd084d7
yamato <mitsu.daigaku@gmail.com>
2026-06-02 14:16:19 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,108 +1,88 @@
 const today = new Date();
 
 document.getElementById("today").innerHTML =
-`${today.getFullYear()}年<br>
-${today.getMonth()+1}月${today.getDate()}日`;
+`${today.getFullYear()}年<br>${today.getMonth()+1}月${today.getDate()}日`;
 
 function openBudget(){
-alert("本日の予算ページは作成中です");
+    alert("本日の予算ページは作成中です");
 }
 
-let todos =
-JSON.parse(localStorage.getItem("todos")) || [];
+function openModal(){
+    document.getElementById("todoModal").style.display="flex";
+}
 
-function saveStorage(){
-localStorage.setItem(
-"todos",
-JSON.stringify(todos)
-);
+function closeModal(){
+    document.getElementById("todoModal").style.display="none";
+}
+
+let todos = JSON.parse(localStorage.getItem("todos")) || [];
+
+function saveTodos(){
+    localStorage.setItem(
+        "todos",
+        JSON.stringify(todos)
+    );
 }
 
 function renderTodos(){
 
-const list =
-document.getElementById("todoList");
+    const list =
+    document.getElementById("todoList");
 
-list.innerHTML="";
+    list.innerHTML = "";
 
-todos.forEach((todo,index)=>{
+    todos.forEach((todo,index)=>{
 
-list.innerHTML += `
-<div class="todo-card">
+        list.innerHTML += `
+        <div class="todo-card">
 
-<div class="number">
-${index+1}
-</div>
+            <div class="number">
+                ${index+1}
+            </div>
 
-<div class="content">
+            <div class="content">
 
-<h3>${todo.title}</h3>
+                <h3>${todo.title}</h3>
 
-<p>${todo.detail}</p>
+                <p>${todo.detail}</p>
 
-<div class="status">
-未完了
-</div>
+                <div class="status">
+                    未完了
+                </div>
 
-</div>
+            </div>
 
-</div>
-`;
-
-});
-
-saveStorage();
-}
-
-function openModal(){
-
-document.getElementById(
-"todoModal"
-).style.display="flex";
-
-}
-
-function closeModal(){
-
-document.getElementById(
-"todoModal"
-).style.display="none";
+        </div>
+        `;
+    });
 
 }
 
 function saveTodo(){
 
-const title =
-document.getElementById(
-"todoTitle"
-).value;
+    const title =
+    document.getElementById("todoTitle").value;
 
-const detail =
-document.getElementById(
-"todoDetail"
-).value;
+    const detail =
+    document.getElementById("todoDetail").value;
 
-if(title.trim()===""){
-return;
-}
+    if(title.trim()===""){
+        return;
+    }
 
-todos.push({
-title:title,
-detail:detail
-});
+    todos.push({
+        title:title,
+        detail:detail
+    });
 
-renderTodos();
+    saveTodos();
 
-document.getElementById(
-"todoTitle"
-).value="";
+    renderTodos();
 
-document.getElementById(
-"todoDetail"
-).value="";
+    document.getElementById("todoTitle").value="";
+    document.getElementById("todoDetail").value="";
 
-closeModal();
-
+    closeModal();
 }
 
 renderTodos();
