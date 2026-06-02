1be135eb293fe93aad827332a471abb9089fdd6c
yamato <mitsu.daigaku@gmail.com>
2026-06-02 12:55:10 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+const today = new Date();
+
+document.getElementById("today").innerHTML =
+`${today.getFullYear()}年<br>
+${today.getMonth()+1}月${today.getDate()}日`;
+
+function openBudget(){
+
+alert(
+`本日の予算
+
+予算ページは作成中です`
+);
+
+}
